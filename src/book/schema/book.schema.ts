6b18b274c6afdd043d1f158ad1268f2e13744b2a
asimpat <asimokon@yahoo.com>
2023-11-19 00:27:15 +0100

    added schema

--- src/book/schema/book.schema.ts
+++ src/book/schema/book.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 import { User } from 'src/auth/schema/user.schema';
 
 export enum Category {
@@ -27,8 +28,8 @@ export class Book {
   @Prop()
   category: Category;
 
-  @Prop()
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   user: User;
 }
 
-export const BookSchema = SchemaFactory.createForClass(Book)
+export const BookSchema = SchemaFactory.createForClass(Book);
